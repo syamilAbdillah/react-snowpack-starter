bdde5323de6462876b3405d7e646d798eb2979fa
syamilAbdillah <abdillahsyamil77@gmail.com>
2021-01-09 02:07:47 +0700

    remove fucking reactstrap

--- src/app.jsx
+++ src/app.jsx
@@ -1,24 +1,11 @@
 import React,{ useState, useEffect} from 'react';
 
 function App (){
-	//create the count state
-	const [count, setCount] = useState(0);
-	//create the counter 
-	useEffect(()=>{
-		const cb = () => setCount(count + 1);
-		const timer = setTimeout(cb, 1000);
-		return () => clearTimeout(timer);
-	},[count, setCount]);
 
-	//return component
 	return(
-		<div className="App">
-			<header className="App-header">
-				<p>
-					page has beeen oper for <code>{count}</code> seconds.
-				</p>
-			</header>
-		</div>
+		<>
+			<h1>Hello</h1>
+		</>
 	);
 }
 
